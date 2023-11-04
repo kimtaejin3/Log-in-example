88741491e2f5576e659a318afa93a6d3884ec7d4
Taejin Kim <rlaxowls1316@naver.com>
2023-11-04 14:47:30 +0900

    Context api 적용

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -5,10 +5,7 @@ export default function MainHeader(props) {
     <header className=" bg-red-300">
       <div className="max-w-5xl mx-auto p-5 flex gap-11 items-center">
         <h1 className="font-bold text-3xl text-white">A Typical Page</h1>
-        <Navigation
-          isLoggedIn={props.isAuthenticated}
-          onLogout={props.onLogout}
-        />
+        <Navigation />
       </div>
     </header>
   );
--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,31 +1,27 @@
+import { useContext } from "react";
 import { AuthContext } from "../../context/auth-context";
 
 export default function Navigation(props) {
+  const ctx = useContext(AuthContext);
   return (
-    <AuthContext.Consumer>
-      {(ctx) => {
-        return (
-          <nav>
-            <ul className="flex gap-5 text-white">
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav>
+      <ul className="flex gap-5 text-white">
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={ctx.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 }
